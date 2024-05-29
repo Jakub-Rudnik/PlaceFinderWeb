8d3bcccc247fa50d59667a8ac9b132befa867318
jakub-rudnik <jakub.rudnik@interia.pl>
2024-05-29 15:24:36 +0200

    Replace temporary data base with service

--- PlaceFinderWeb/Pages/Index.cshtml.cs
+++ PlaceFinderWeb/Pages/Index.cshtml.cs
@@ -1,24 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using PlaceFinderWeb.Models;
+using PlaceFinderWeb.Services;
 
 namespace PlaceFinderWeb.Pages;
 
 public class IndexModel : PageModel
 {
     private readonly DatabaseContext _context;
+    private PlaceService _placeService;
     
     public List<Place> Places { get; set; }
     
     public IndexModel(DatabaseContext context)
     {
-        _context = context;
+        _placeService = new PlaceService(context);
 
         
         
-        Places = _context.Places.ToList();
-        
-        _context.SaveChanges();
+        Places = _placeService.ReadAll();
+
         
     }
 
