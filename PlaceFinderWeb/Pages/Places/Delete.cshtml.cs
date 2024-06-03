1c84834b49ccf06396a79fe3302d81df994ee74a
jakub-rudnik <jakub.rudnik@interia.pl>
2024-06-03 15:08:42 +0200

    fix create site and added edit and delete pages

---
+++ PlaceFinderWeb/Pages/Places/Delete.cshtml.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using PlaceFinderWeb.Models;
+using PlaceFinderWeb.Persistence;
+
+namespace PlaceFinderWeb.Pages.Places;
+
+public class Delete : PageModel
+{
+    private readonly PlaceFinderDbContext _dbContext;
+
+    public Delete(PlaceFinderDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    [BindProperty]
+    public Place Place { get; set; }
+    public string ErrorMessage { get; set; }
+
+    public async Task<IActionResult> OnGetAsync(int? id, bool? saveChangesError = false)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        Place = await _dbContext.Places
+            .AsNoTracking()
+            .FirstOrDefaultAsync(p => p.Id == id);
+
+        if (Place == null)
+        {
+            return NotFound();
+        }
+
+        if (saveChangesError.GetValueOrDefault())
+        {
+            ErrorMessage = String.Format("Delete {ID} failed. Try again", id);
+        }
+
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPostAsync(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var student = await _dbContext.Places.FindAsync(id);
+
+        if (student == null)
+        {
+            return NotFound();
+        }
+
+        try
+        {
+            _dbContext.Places.Remove(student);
+            await _dbContext.SaveChangesAsync();
+            return RedirectToPage("/Index");
+        }
+        catch (DbUpdateException ex)
+        {
+            return RedirectToAction("/Places/Delete",
+                new { id, saveChangesError = true });
+        }
+    }
+}
---
+++ PlaceFinderWeb/Pages/Places/Edit.cshtml.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using PlaceFinderWeb.Models;
+using PlaceFinderWeb.Persistence;
+
+namespace PlaceFinderWeb.Pages.Places;
+
+public class Edit : PageModel
+{
+    private readonly PlaceFinderDbContext _dbContext;
+
+    public Edit(PlaceFinderDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    [BindProperty]
+    public Place Place { get; set; } = default!;
+
+    public async Task<IActionResult> OnGetAsync(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        Place = await _dbContext.Places.FindAsync(id);
+
+        if (Place == null)
+        {
+            return NotFound();
+        }
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPostAsync(int id)
+    {
+        var placeToUpdate = await _dbContext.Places.FindAsync(id);
+
+        if (placeToUpdate == null)
+        {
+            return NotFound();
+        }
+
+        if (await TryUpdateModelAsync<Place>(
+                placeToUpdate,
+                "place",
+                p => p.Name, p => p.Description, p => p.Latitude, p => p.Longitude))
+        {
+            await _dbContext.SaveChangesAsync();
+            return RedirectToPage("/Index");
+        }
+
+        return Page();
+    }
+}
